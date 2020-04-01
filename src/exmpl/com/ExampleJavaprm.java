29fc1c4112162545071ca9a4ab7424634f76b369
tharagold <kanagathara60@gmail.com>
2020-04-01 00:41:48 -1000

    new sample java program

---
+++ src/exmpl/com/ExampleJavaprm.java
@@ -0,0 +1,12 @@
+package exmpl.com;
+
+public class ExampleJavaprm {
+public void a() {
+	// TODO Auto-generated method stub
+System.out.println("welcome");
+}
+public static void main(String[] args) {
+	ExampleJavaprm s=new ExampleJavaprm();
+	s.a();
+}
+}
